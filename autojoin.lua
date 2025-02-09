9ca4b39bc5d77205da6a31fb3ef9ee5eb819b799
Bankz12 <106497181+Bankz12@users.noreply.github.com>
2025-02-08 23:14:00 -0500

    Create autojoin.lua

---
+++ autojoin.lua
@@ -0,0 +1,5 @@
+token = "MTI3MDc1Mzk5Nzc5OTU1NTE2Ng.GpDIAA.xVY08SdlC-4Qn5TAtAIiLAH1czRi5wK7ERcWVM" 
+channelId = "1335352447702405282" 
+delay = 2 
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/MM2/refs/heads/main/autojoiner.lua'))()
